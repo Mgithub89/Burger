8e7c33e440f51c58473fb45df083efc0739867c6
metages worku <metages09@gmail.com>
2020-12-07 11:32:05 -0800

    hooking the project with jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql");
 require("dotenv").config();
-
+var connection;
 //create database connection
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DB
+    });
+};
 //connect to mysql server
 connection.connect(function (err) {
     if (err) {
